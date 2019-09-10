7e4aaa581f404bd3ae2b08f4a20dec6feb2b9b9b
Albert Camacho <camacho.albert82@gmail.com>
2019-09-10 18:26:55 -0400

    align
    align some lines

--- post-script/partition_an_mount_disk.sh
+++ post-script/partition_an_mount_disk.sh
@@ -5,7 +5,7 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/xvdc
   p # primary partition
   1 # partition number 1
     # default - first sector of disk
-	# default - last sector of disk
+    # default - last sector of disk
   w # write the partition table
 EOF
 
